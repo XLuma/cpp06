18158c0c1db30cec3aff5be8cff608940bde49fa
Louis-gabriel Laplante <llaplant@c3r8p5.42quebec.com>
2022-06-28 10:46:16 -0400

    made it so ex00 never crashes

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -14,8 +14,13 @@ int main(int argc, char **argv)
 		std::cout << "No viable argument provided ! Usage: ./convert [arg]" << std::endl;
 		return 0;
 	}
-	conversion.detect_type(argv[1]);
-	conversion.convert_data();
-	conversion.print_data();
+	try {
+		conversion.detect_type(argv[1]);
+		conversion.convert_data();
+		conversion.print_data();
+	}
+	catch (std::exception &e) {
+		std::cout << "Conversion aborted. Cannot go bigger than a long !" << std::endl;
+	}
 	return 0;
 }
